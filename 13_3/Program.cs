aa59b6ee6f2240ad3e6763b9c3c8eb769cddc873
Арсений Мазуренко <arseniy.mazurenko.03@gmail.com>
2022-12-14 16:58:04 +0300

    Добавьте файлы проекта.

---
+++ 13_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    public static int count = 0;
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(Increment);
+        Thread thread2 = new Thread(Increment);
+
+        thread.Start();
+        thread2.Start();    
+
+        void Increment()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                count++;
+            }          
+        }
+
+        Thread.Sleep(1000);
+        Console.WriteLine(count);
+    }
+}
